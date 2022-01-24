6d891da64763a536db6e4c0924851507a1a8a69b
ManuelExposito <manuelexposito4d@gmail.com>
2022-01-24 14:11:19 +0100

    Dependencia JUnit actualizada

---
+++ src/test/java/com/sopromadze/blogapi/repository/UserRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.sopromadze.blogapi.repository;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    
+
+}
