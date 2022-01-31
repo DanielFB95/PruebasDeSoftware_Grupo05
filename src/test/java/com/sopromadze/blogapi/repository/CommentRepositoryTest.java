43b99afb42523849ffa3f60e880d95310bc9bef5
Manuel Exposito <manuelexposito4d@gmail.com>
2022-01-31 17:06:28 +0100

    Cambios menores

--- src/test/java/com/sopromadze/blogapi/repository/CommentRepositoryTest.java
+++ src/test/java/com/sopromadze/blogapi/repository/CommentRepositoryTest.java
@@ -8,7 +8,10 @@ import com.sopromadze.blogapi.model.user.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
+
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -16,22 +19,24 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CommentRepositoryTest {
 
-    @MockBean
-    TestEntityManager testEntityManager;
+    @Autowired
+    private TestEntityManager testEntityManager;
 
-    @MockBean
-    CommentRepository commentRepository;
-
-
+    @Autowired
+    private CommentRepository commentRepository;
 
     User user;
     Comment comment;
@@ -40,20 +45,21 @@ class CommentRepositoryTest {
     @BeforeEach
     void setUp() {
         user = User.builder()
-                .id(1L)
-                .firstName("Juan")
-                .lastName("Gutierrez")
-                .username("Juanguti")
-                .password("lacontrasenadeljuan")
-                .email("juanguti@gmail.com")
+                .firstName("user2")
+                .lastName("user2")
+                .username("useruser")
+                .password("contrasena")
+                .email("user@gmail.com")
                 .comments(new ArrayList<>())
                 .roles(new ArrayList<>())
                 .build();
 
-
         user.getRoles().add(new Role(RoleName.ROLE_USER));
+
+        user.setCreatedAt(Instant.now());
+        user.setUpdatedAt(Instant.now());
+
         comment = Comment.builder()
-                .id(1L)
                 .body("Este post ha cambiado mi vida por completo")
                 .name(user.getFirstName())
                 .user(user)
@@ -61,7 +67,6 @@ class CommentRepositoryTest {
                 .build();
 
         post = Post.builder()
-                .id(1L)
                 .title("Mi viaje a las Bahamas")
                 .body("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum")
                 .comments(new ArrayList<>())
@@ -72,19 +77,23 @@ class CommentRepositoryTest {
         post.getComments().add(comment);
         user.getComments().add(comment);
         comment.setPost(post);
+
+        testEntityManager.persist(post);
+        testEntityManager.persist(user);
+        testEntityManager.persist(comment);
+
     }
 
+    //TODO: Solucionar esta prueba
     @Test
     void findByPostId_success() {
+
 
         Page<Comment> result = new PageImpl<>(Arrays.asList(comment));
 
         Pageable pageable = PageRequest.of(1, 1);
 
-        when(commentRepository.findByPostId( 1L, pageable)).thenReturn(result);
-
-        assertEquals(result, commentRepository.findByPostId(1L, pageable));
-
+        assertEquals(result, commentRepository.findByPostId(comment.getId(), pageable));
 
     }
 }
