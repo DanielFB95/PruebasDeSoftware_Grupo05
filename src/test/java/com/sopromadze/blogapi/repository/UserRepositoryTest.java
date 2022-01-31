12d0838198d8f2f2f8f481476648619c29d9b773
Manuel Exposito <manuelexposito4d@gmail.com>
2022-01-31 16:19:54 +0100

    Pruebas de UserRepository agregadas

--- src/test/java/com/sopromadze/blogapi/repository/UserRepositoryTest.java
+++ src/test/java/com/sopromadze/blogapi/repository/UserRepositoryTest.java
@@ -1,15 +1,20 @@
 package com.sopromadze.blogapi.repository;
 
+import com.sopromadze.blogapi.exception.ResourceNotFoundException;
 import com.sopromadze.blogapi.model.Comment;
 import com.sopromadze.blogapi.model.Post;
 import com.sopromadze.blogapi.model.role.Role;
 import com.sopromadze.blogapi.model.role.RoleName;
 import com.sopromadze.blogapi.model.user.User;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -20,6 +25,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -54,17 +60,53 @@ class UserRepositoryTest {
         user.setUpdatedAt(Instant.now());
 
 
+        testEntityManager.persist(user);
+
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("getDifferentStrings")
+    @DisplayName("Encontrar un usuario por Username o por su email.")
+    void findByUsernameOrEmail_success(String username, String email) {
+
+
+        assertEquals(Optional.of(user), userRepository.findByUsernameOrEmail(username, email));
 
 
     }
 
+    private static Stream<Arguments> getDifferentStrings() {
+
+        User mockUser = User.builder()
+                .username("Juanguti")
+                .email("juanguti@gmail.com")
+                .comments(new ArrayList<>())
+                .roles(new ArrayList<>())
+                .build();
+
+        return Stream.of(
+                Arguments.arguments(mockUser.getUsername(), mockUser.getEmail()), //Pasando username y email
+                Arguments.arguments(mockUser.getUsername(), null), //Pasando username, pero no email
+                Arguments.arguments(null, mockUser.getEmail())//Pasando email, pero no username
+        );
+    }
 
     @Test
-    void findByUsernameOrEmail_success() {
+    @DisplayName("Encontrar a un usuario buscado por su Username.")
+    void getUserByName_success(){
 
-        testEntityManager.persist(user);
-        assertEquals(Optional.of(user), userRepository.findByUsernameOrEmail(user.getUsername(), user.getEmail()));
-
+        assertTrue(userRepository.getUserByName(user.getUsername()).equals(user));
 
     }
+
+    @Test
+    @DisplayName("Lanzar excepción ResourceNotFoundException si no encuentra al usuario.")
+    void getUserByName_ThrowExceptionNotFound(){
+
+        assertThrows(ResourceNotFoundException.class, () -> userRepository.getUserByName("userNoExistente"));
+    }
+
+
+
 }
